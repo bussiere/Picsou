0b8048dee21a90bc976b4fb8ad20f8be15dadaa8
bussiere <bussiere@gmail.com>
2012-12-30 15:15:13 +0100

    rajout

--- Picsou/settings.py
+++ Picsou/settings.py
@@ -11,19 +11,19 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+#DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
-if bool(os.environ.get('LOCAL_DEV', False)):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'picsou',                      # Or path to database file if using sqlite3.
-            'USER': 'toto',                      # Not used with sqlite3.
-            'PASSWORD': 'titi',                  # Not used with sqlite3.
-            'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }
+#if bool(os.environ.get('LOCAL_DEV', False)):
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'picsou',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
+}
 
 
 # Local time zone for this installation. Choices can be found here:
