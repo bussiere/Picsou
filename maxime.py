51bd0ec3ba41f12697ebf999bd70bda72f9c08ce
bussiere <bussiere@gmail.com>
2012-12-30 18:02:09 +0100

    merge

--- maxime.py
+++ maxime.py
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 import urllib
 import simplejson
 
 
 def extract_hash_tags(s):
 	return set(part[1:] for part in s.split() if part.startswith('#'))
+
 
 def searchTweets(query):
  search = urllib.urlopen("http://search.twitter.com/search.json?q="+query)
@@ -19,24 +19,4 @@ def searchTweets(query):
  		print i
 
 
-searchTweets("apple%20stockvalue")
-
-
-=======
-import urllib
-import simplejson
-
-
-def searchTweets(query):
- search = urllib.urlopen("http://search.twitter.com/search.json?q="+query)
- dict = simplejson.loads(search.read())
- for result in dict["results"]: 
- # result is a list of dictionaries
- 	print result['from_user_name'].encode('utf-8'),"\n"
- 	print "*",result["text"].encode('utf-8'),"\n"
-
-
 searchTweets("#apple")
-
-
->>>>>>> 1aefabbbfc1ea8e659dd0829a77e68c588af6355
