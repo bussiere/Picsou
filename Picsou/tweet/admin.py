7245b9770dd7a537b4957037867012bf8e1c1765
bussiere <bussiere@gmail.com>
2012-12-31 00:00:13 +0100

    maxime

--- Picsou/tweet/admin.py
+++ Picsou/tweet/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from tweet.models import Auteur,HashTag,Tweet,OldTweet
+from tweet.models import Auteur,HashTag,Tweet,OldTweet,MotClef
 admin.site.register(Auteur)
 admin.site.register(HashTag)
 admin.site.register(Tweet)
 admin.site.register(OldTweet)
+admin.site.register(MotClef)
 
 
--- Picsou/tweet/models.py
+++ Picsou/tweet/models.py
@@ -6,14 +6,17 @@ class Auteur(models.Model):
     Nom = models.TextField(max_length=50, null=True, blank=True)
     Note = models.TextField(max_length=1024, null=True, blank=True)
     Amis = models.ManyToManyField('Auteur',null=True, blank=True)
-    Puissance =  models.FloatField()
+    Puissance =  models.FloatField(null=True, blank=True)
     def __unicode__(self):
-        return str(self.Nom)
+        return str(self.Nom.decode('utf-8'))
+    def __str__(self):
+        return str(self.Nom.decode('utf-8'))
+
     
 class HashTag(models.Model):
-    HahTag = models.TextField(max_length=50, null=True, blank=True)
+    HashTag = models.TextField(max_length=50, null=True, blank=True)
     def __unicode__(self):
-        return str(self.HahTag)
+        return str(self.HashTag)
 
 class MotClef(models.Model):
     MotClef = models.TextField(max_length=50, null=True, blank=True)
@@ -24,18 +27,21 @@ class Tweet(models.Model):
     Auteur = models.ForeignKey(Auteur, unique=False, null=True, blank=True)
     Contenu = models.TextField(max_length=200, null=True, blank=True)
     Analyse = models.BooleanField(default=False)
-    HasTag = models.ManyToManyField('HashTag')
+    HashTag = models.ManyToManyField('HashTag')
     Salt = models.TextField(max_length=200, null=True, blank=True)
     Date = models.DateTimeField(auto_now_add=True, blank=True)
     Recherche = models.TextField(max_length=200, null=True, blank=True)
+    RT = models.BooleanField(default=False)
+    Auteur_RT = models.ManyToManyField('Auteur', unique=False, null=True, blank=True,related_name="Auteur RT")
+    Contient = models.ManyToManyField('Auteur', unique=False, null=True, blank=True,related_name="contient")
     def __unicode__(self):
-        return str(self.Auteur + " "+ self.Contenu)
+        return str(self.Auteur.Nom + " "+ self.Contenu)
 
 class OldTweet(models.Model):
     Auteur = models.ForeignKey(Auteur, unique=False, null=True, blank=True)
     Contenu = models.TextField(max_length=200, null=True, blank=True)
     Analyse = models.BooleanField(default=False)
-    HasTag = models.ManyToManyField('HashTag')
+    HashTag = models.ManyToManyField('HashTag')
     Salt = models.TextField(max_length=200, null=True, blank=True)
     Date = models.DateTimeField(auto_now_add=True, blank=True)
     Recherche = models.TextField(max_length=200, null=True, blank=True)
